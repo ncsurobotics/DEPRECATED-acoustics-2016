7bd909ee06c1ce76b4e1a144517535dfd27f42ab
root <root@beaglebone.(none)>
2014-12-29 00:11:10 +0000

    Established testing loop program.

--- Apps/pinger_finder/ADS7865_sample.p
+++ Apps/pinger_finder/ADS7865_sample.p
@@ -8,6 +8,14 @@ START:
         CLR  r0, r0, 4          // Clear bit 4 in reg 0
         SBCO r0, C4, 4, 4       // Store Bytes Constant Offset
 
+        MOV  r0, 0x2FAF0800
+        MOV  r2, 0
+ROADBLOCK:
+        QBEQ END, r0, 0
+        SUB  r0, r0, 1
+//        LBBO r1, r2, 0, 4
+        QBA  ROADBLOCK
+        //QBEQ ROADBLOCK, r1, 1
 END:
         MOV r31.b0, PRU0_ARM_INTERRUPT+16 // Send notification to host for program completion
         HALT
--- Apps/pinger_finder/main.py
+++ Apps/pinger_finder/main.py
@@ -1,22 +1,32 @@
 import pypruss
 import mmap
 import struct
-
+import time
 
 def main():
-	#Initialize evironment
-	pypruss.modprobe()
-	
+	# Initialize evironment
+	pypruss.modprobe()	
 	pypruss.init()		# Init the PRU
 	pypruss.open(0)		# Open PRU event 0 which is PRU0_ARM_INTERRUPT
 	pypruss.pruintc_init()  # Init the interrupt controller
+
+	# Configure PRU Registers
+	pypruss.pru_write_memory(0, 0, [0,])
+	
+	# Execute the PRU program
+	a = time.time()
 	pypruss.exec_program(0, "./ADS7865_sample.bin") # Load firmware on PRU0
+
+	# Wait for PRU to finish its job.
 	pypruss.wait_for_event(0)# Wait for event 0 which is conn to PRU0_ARM_INTERUPT
+	loop_time = time.time() - a
+
+	# Once signal has been received, clean up house
 	pypruss.clear_event(0)	# Clear the event
 	pypruss.exit()		# Exit PRU
 
-	print("Test completed: PRU was successfully opened and closed.")
+	print("That's a %ds loop you have there." % loop_time)
+	print("done")
 
 print("-"*50)
-print("test.py: testing basic functionality...")
 main()

0f12d52ca2c34933675a0bc98e3c6fd7c5a3d5a3
Josh Smith <jrsmit23@ncsu.edu>
2015-01-19 16:14:18 -0500

    Established skeleton code for the communication app.

--- Apps/Host_Communication_App/main.py
+++ Apps/Host_Communication_App/main.py
@@ -6,7 +6,26 @@ print("Acoustics Client Communication App")
 
 
 def main():
+	# Initialize connection
+	ser = serial.Serial("/dev/tty.usbserial-A5025L80", 9600)
 	
+	# Check if successful
+	cmd_helloBBB()
+	response = ser.readline()
+	if hello in response:
+		print(response)
+	else:
+		print("main: Hmmm. I didn't get any response from the beaglebone... Check your connections.")
+		exit(1)
+		
+	# Connection successful. No going to run through a list of things the BBB should be able to handle.
+	cmd_echo('string')
+	cmd_burst(30)
+	cmd_burst()
+	cmd_seekAndBurst(30)
+	cmd_seekAndBurst()
+	cmd_configure('burst_size')
+	cmd_readDataBlock()
 
 # Set up the program, or grant user help if he needs it.
 argc = len(argv)

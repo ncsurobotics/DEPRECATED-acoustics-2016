4d2979c722508d50303d2f7789fc990a43ced878
BBB <iamsmithxd@gmail.com>
2015-01-25 11:27:22 -0500

    Got acoustics communication app to work on the beaglebone.

---
+++ Apps/Host_Communication_App/README.md
@@ -0,0 +1,48 @@
+# Host Communication App
+
+This is meant to be the program that seawolf will run in order to 
+send and recieve commands/data to/from the beaglebone.
+
+This is a WIP. Below is a list of features that need to be implemented in
+in order to complete this project:
+
+[ ] Remotely run any program found on the beaglebone using this app.
+
+[ ] Remotely request and recieve data from the beaglebone using this app.
+
+## How to run the program
+
+Make sure the [FT232RL Breakout board](https://www.sparkfun.com/products/12731)
+ is connected to the host computer. Also, make sure the board's breakout pins
+are connected such that the beaglebone can at least transmit UART messages
+ to the breakout board. then simply run `python main.py` from
+ the terminal screen (after you cd into this directory) to run the program.
+
+As of the current state of this program. A screen will appear to say
+"listening" which means you can transmit UART messages from the
+beaglebone and see see them displayed on the host's terminal screen. 
+If you would like test this, simply open another terminal screen to
+act like the client beaglebone, and follow the steps from the "how
+to write UART from the beaglebone" section below. 
+
+## How to write UART from the beaglebone:
+
+Manually:
+
+The manual method involves running this list of commands:
+
+```
+root@beaglebone:~# config-pin -a P9.13 uart
+```
+
+This will enable the uart tx on  pin 13 header 9. (If you're ever unsure
+what a pin does, you have access to the `config-pin -q <pin>` or 
+`config-pin -i <pin>` commands, as well as the master UART table that 
+can be soon found in the wiki of this github)
+
+Now, sending commands is a matter of "echoing" text into the correct 
+file representing the UART system:
+
+```
+root@beaglebone:~# echo --insert message here-- > /dev/ttyO4
+```
--- Apps/Host_Communication_App/main.py
+++ Apps/Host_Communication_App/main.py
@@ -1,16 +1,41 @@
 import serial
 from sys import argv
+import platform
 
 print('-'*50)
 print("Acoustics Client Communication App")
-ser = serial.Serial("/dev/tty.usbserial-A5025L80", 9600)
+
+# Connect to the UART-to-USB device
+if "bone" in platform.platform():
+	device = "/dev/ttyUSB0"
+	ser = serial.Serial("/dev/ttyUSB0", 9600)
+else:
+	print("Com_App: I'm not sure what" +
+		" the device is called on "+
+		"your system, so I'm assuming " +
+		"/dev/tty.usbserial-A5025L80")
+	device = "/dev/tty.usbserial-A5025L80"
+
+
+ser = serial.Serial(device, 9600)
 
 
 def cmd_listen():
 	print("listening...")
 	
 	while True:
-		print(ser.readline())
+		print repr(ser.readline()[0:-2])
+
+
+def cmd_unit_test():
+	cmd_echo('string')
+	cmd_burst(30)
+	cmd_burst()
+	cmd_seekAndBurst(30)
+	cmd_seekAndBurst()
+	cmd_configure('burst_size')
+	cmd_readDataBlock()
+
 
 
 def main():
@@ -22,26 +47,26 @@ def main():
 	# Check if successful
 	cmd_helloBBB()
 	response = ser.readline()
-	if hello in response:
+	if "hello" in response:
 		print(response)
 	else:
-		print("main: Hmmm. I didn't get any response from the beaglebone... Check your connections.")
+		print("main: Hmmm. I didn't get any response from the" + 
+			" beaglebone... Check your connections.")
 		exit(1)
 		
-	# Connection successful. No going to run through a list of things the BBB should be able to handle.
-	cmd_echo('string')
-	cmd_burst(30)
-	cmd_burst()
-	cmd_seekAndBurst(30)
-	cmd_seekAndBurst()
-	cmd_configure('burst_size')
-	cmd_readDataBlock()
+	# Connection successful. Now going to run through a list of things
+	#  the BBB should be able to handle.
+	cmd_unit_test()
+
+#########################################
+########  System Call Handline   ########
+########################################
 
 # Set up the program, or grant user help if he needs it.
 argc = len(argv)
 if argc == 2:
 	if 'h' in argv[1]:
-		print("help text.")
+		print("--insert helpful text here--")
 		exit(1)
 
-main()
+main() #Run the main function ^^^^^^

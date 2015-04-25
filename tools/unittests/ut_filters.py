6bfaf87bfaaf2083567cadd550d9df45dd3191c4
BBB <iamsmithxd@gmail.com>
2015-04-25 16:03:00 -0400

    Filter unit test code.

--- tools/unittests/ut_filters.py
+++ tools/unittests/ut_filters.py
@@ -3,16 +3,16 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 
 import numpy as np
 
-def gain_test(ADC,filt,plt): "Testing gain"
+def gain_test(ADC,filt,plt): #Testing gain
 	"Init some empty data structures"
-	legend_list = [''] * ADC_obj.n_channels
-	Vpp	= [0] * ADC_obj.n_channels
+	legend_list = [''] * ADC.n_channels
+	Vpp = [0] * ADC.n_channels
 
 	print("ut_filters: Gain test. Be sure to:\n"
-	+ "  * Attach an input sinusoid on all four channels.\n"
-	+ "  * Input frequency is low compared to the cutoff frequency.\n"
-	+ "  * Input signal amplitude < 156mV.\n"
-	+ "  * Ensure settings are correct on ADC.\n")
+	+ "	 * Attach an input sinusoid on all four channels.\n"
+	+ "	 * Input frequency is low compared to the cutoff frequency.\n"
+	+ "	 * Input signal amplitude < 156mV.\n"
+	+ "	 * Ensure settings are correct on ADC.\n")
 	
 	"Arm the ADC"
 	ADC.Ready_PRUSS_For_Burst()
@@ -28,16 +28,18 @@ def gain_test(ADC,filt,plt): "Testing gain"
 		fig,axes = plt.subplots(4,4,sharex=True)
 	
 	"Iterate analysis 16 times"
-	for (n in range(16)): #16=num of possible gain values
+	for n in range(16): #16=num of possible gain values
+		r = n % 4
+		c = n/4
 		if plt:
 			"hold the axis"
-			ax.hold(True)
+			axes[r,c].hold(True)
 			
-		"Select gain mode
+		"Select gain mode"
 		filt.GainMode(n)
 		
 		"Get samples"
-		y = ADC.Burst()
+		y,f_time = ADC.Burst()
 			
 		"Analyze each sample"
 		for chan in range(ADC.n_channels):
@@ -46,38 +48,43 @@ def gain_test(ADC,filt,plt): "Testing gain"
 		
 			"Compute plot"
 			if plt:
-				"Generate time array
+				"Generate time array"
 				if chan is 0:
 					M = y[0].size
-					t = ADC_obj.Generate_Matching_Time_Array(M)
+					t = ADC.Generate_Matching_Time_Array(M)
 		
 				"Record plot name"
 				legend_list[chan] = ADC.ch[chan]
 				
-				"plot
+				"plot"
 				plt.plot(t,y[chan])
 		
 		"Show off math"
 		print("ut_filters: Your amplitudes for G = %d were:" % n)
 		for chan in range(ADC.n_channels):
-			print("  %s: %.2fV " % (ADC.ch[chan], Vpp[chan]))
+			print("	 %s: %.2fV " % (ADC.ch[chan], Vpp[chan]))
 		print("")
 		
-		"slap on the plot legend & name"
-		ax.title('Signal at G = %d' % n)
-		ax.legend(legend_list)
+		if plt:
+			"slap on the plot legend & name"
+			axes[r,c].set_title('Signal at G = {}'.format(n))
+			import pdb; pdb.set_trace()
+			axes[r,c].legend(legend_list)
 		
 	"Show plt"
 	if plt:
 		plt.ion()
 		plt.show()
+		
+	"Return filter back to zero"
+	filt.GainMode(0)
 	
-	return:
+	return
 		
 		
 	
 
-def test(ADC=None, filt=None, plt=None):
+def testFilts(ADC=None, filt=None, plt=None):
 	if ADC is None:
 		print("ut_filters: You haven't activated your ADC yet!")
 		return;

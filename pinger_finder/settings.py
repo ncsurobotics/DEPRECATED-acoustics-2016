3fce246e81ff47f2af0030076fe5d82e833625ab
BBB <iamsmithxd@gmail.com>
2015-04-29 18:53:39 -0400

    small typo

--- pinger_finder/settings.py
+++ pinger_finder/settings.py
@@ -1,6 +1,6 @@
 from os import path
 
-# Get va
+# Get variables for navigating the file system.
 root_directory = path.dirname(path.dirname(path.realpath(__file__)))
 pf_directory = path.join(root_directory,"pinger_finder")
 bin_directory = path.join(pf_directory,"pru/bin/")

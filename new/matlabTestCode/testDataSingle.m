4b77c271354e103093d049117fac8aa226950935
Rafael <riestell@ncsu.edu>
2017-10-31 09:34:30 -0400

    added code for running through all of the test data in a file at once, and code for looking at an individual function. Modified sigToa so that it only outputs graph when desired aka when in debugging mode

---
+++ new/matlabTestCode/testDataSingle.m
@@ -0,0 +1,26 @@
+filename = 'data/conf1.csv';
+data = csvread(filename, 7, 00);
+data = data(:, 1:4);
+t = 1:length(data);
+figure(1)
+plot(t, data);
+%%
+% the frequency of sampling
+Fs = 125 * 10^6;
+%the frequency of the pinger
+pf = 22 * 10^3;
+% the starting point of the input
+i= 2.5 * 10^5;
+%size of the input
+inputSize = 5*10^4;
+
+t = uint64(i:1:(i + inputSize));
+toa = data(t, :);
+figure(2)
+plot(toa)
+out = testToa3d(toa, Fs, pf, true);
+fprintf('length, toaCalc, a, b, yaw\n');
+fprintf(out{1});
+fprintf('length, toaCalc, a, b, pitch\n');
+fprintf(out{2});
+

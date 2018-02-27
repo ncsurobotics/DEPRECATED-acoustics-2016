40ebaf957d7485d376788a684ca111bbf21f905a
rafael <riestell@ncsu.edu>
2018-02-26 20:00:55 -0500

    added tesYaw.m, runs through various degrees to test code. output is inconsistant for some reason????

--- new/matlabTestCode/signalToa2.m
+++ new/matlabTestCode/signalToa2.m
@@ -21,6 +21,7 @@ t2 = t(index2:(M+index2));
 % the input read in from the hydrophone, assuming 0 offset
 signal = [cos(2 * pi * ( pf) * t1); cos(2 * pi * pf * t2)];
 %% plotting the output wave
+%{
 figure()
 hold on
 plot(signal(1,:))
@@ -28,7 +29,8 @@ plot(signal(2,:));
 hold off
 title('plot of signal')
 ylabel('amplitude of sign')
-xlabel('time in secconds')
+xlabel('time in secconds') 
+%}
 %%
 Y = [fft(signal(1,:));fft(signal(2,:))];
 [~,i] = max(abs(Y'));
@@ -42,7 +44,7 @@ calcToa = (angle(Y(2, i(2))) - angle(Y(1, i(1))))/(pf * 2 * pi);
 %err = %abs(toa - calcToa)/toa;
 err = 0;
 
-fprintf('IN: %6.2f TDOA: %6.2f, TDOA error: %6.2f%%\n', toa * 10^5, calcToa * 10^5, err * 100);
+%fprintf('IN: %6.2f TDOA: %6.2f, TDOA error: %6.2f%%\n', toa * 10^5, calcToa * 10^5, err * 100);
 end
 
 
